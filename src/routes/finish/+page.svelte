909ab5f0d2a7378347b5964400fa7be5b9b62eba
Owen Shaule <owen.shaule@gmail.com>
2023-12-08 00:39:58 -0500

    another embed

--- src/routes/finish/+page.svelte
+++ src/routes/finish/+page.svelte
@@ -33,5 +33,8 @@
                 </Link>
             </ListItem>
         </UnorderedList>
+        <br />
+        <br />
+        <iframe style="width:100%;aspect-ratio:16/9;" src="https://www.youtube.com/embed/PVL24HAesnc" title="Polar &amp; Non-Polar Molecules: Crash Course Chemistry #23" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </div>
 </Content>
