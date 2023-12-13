39171ab3b911b75e87d070dd9989529e7cb79b7f
Owen Shaule <owen.shaule@gmail.com>
2023-12-13 09:20:39 -0500

    oops

--- src/lib/3d/Scene3.svelte
+++ src/lib/3d/Scene3.svelte
@@ -3,7 +3,7 @@
 	import NonPolar from "./NonPolar.svelte";
 </script>
 
-<Circle color="#f1c21b" position={[0, 0.75, 0]} size={0.55} />
+<Circle color="#f1c21b" position={[0, 1, 0]} size={0.55} />
 <Circle color="#d63366" position={[0, 0.75, -1]} size={0.75} />
 <Circle color="#d63366" position={[0, 0.75, 1]} size={0.75} />
 
